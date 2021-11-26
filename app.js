4184f4a4b571c2a9a58e4a816d8171b7f4e3630a
Jamie Cole <jamiejcole@icloud.com>
2021-11-26 18:43:40 +1100

    it works

---
+++ app.js
@@ -0,0 +1,47 @@
+const timerText = document.getElementById("timerText");
+
+var length = 0;
+var currentTimeMinutes = 25;
+var currentTimeSecs = 0;
+var running = false;
+
+function setTime(time) {
+	if (running == false) {
+		length = time;
+		document.getElementById("timerText").innerHTML = time + ":00";
+	}
+}
+
+function runTimer() {
+	if (running == false) {
+		document.getElementById("timerText").innerHTML = length - 1 + ":59";
+		running = true;
+		var countDownDate = new Date();
+		countDownDate.setMinutes(countDownDate.getMinutes() + length)
+
+		// Update the count down every 1 second
+		var x = setInterval(function() {
+
+			// Get today's date and time
+			var now = new Date().getTime();
+
+			// Find the distance between now and the count down date
+			var distance = countDownDate - now;
+
+			// Time calculations for days, hours, minutes and seconds
+			var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+			var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+			var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+			var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+			// Display the result in the element with id="demo"
+			document.getElementById("timerText").innerHTML = minutes + ":" + seconds;
+
+			// If the count down is finished, write some text
+			if (distance < 0) {
+				clearInterval(x);
+				document.getElementById("timerText").innerHTML = "EXPIRED";
+			}
+		}, 1000);
+	}
+}
